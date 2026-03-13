f1782d8c80a6862e5b65d0fbde7930a8860eac6e
armaan9991 <armaangill9991@gmail.com>
2026-03-13 15:11:35 -0600

    testing remote connection

--- BottleDepot/Controllers/WeatherForecastController.cs
+++ BottleDepot/Controllers/WeatherForecastController.cs
@@ -1,26 +1,2 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace BottleDepot.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
-    {
-        private static readonly string[] Summaries =
-        [
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        ];
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-    }
-}
+// need to work on this..
+// Armaan G
