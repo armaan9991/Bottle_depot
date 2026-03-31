f4a4f9ae5d13af21c859cc9737aa16a1f995f1df
Armaan Gill <armaan.gill1@ucalgary.ca>
2026-03-30 22:44:53 -0600

    implemented routes to app.js to navigagte to pages of admin

--- BottleDepot/UI/src/App.jsx
+++ BottleDepot/UI/src/App.jsx
@@ -1,10 +1,57 @@
+import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+/*
+NEED  TO WRITE ROUTES STILL.
+*/
+
+
+
 export default function App(){
   const {user} = useAuth();
 
   return(
     <div>
-      { 
+    <Route>
+      <Route path="/" element={<Login/>} />      /* Route back to login */
+      
+      <Route path="/ " element={                // route to Admin Dashboard  
+        <ProtectedRoute role="Admin">
+          <AdminDashBoard />
+        </ProtectedRoute>
+        }
+      />
+
+      <Route path="" element={
+        <ProtectedRoute role="Admin">
+          <DailyRecord/>
+        </ProtectedRoute>
+        }
+      />
+
+      <Route path="" element={
+        <ProtectedRoute role="Admin">
+          <ManageEmployees/>
+        </ProtectedRoute>
+        }
+      />
+
+      <Route path="" element={
+        <ProtectedRoute role="Admin">
+          <ManageShipments/>
+        </ProtectedRoute>
+        }
+      />
+
+      <Route path="" element={
+        <ProtectedRoute role="Admin">
+          <Reports/>
+        </ProtectedRoute>
+        }
+      />
+
+    </Route>      
       
     <Login />
     </div>
