5387456482c4ca87e2832889aa05f0571340b49c
30233529 <harjassingh.anand@ucalgary.ca>
2026-03-31 23:13:57 -0600

    implemented auth.js and login.css

--- BottleDepot/UI/src/api/auth.js
+++ BottleDepot/UI/src/api/auth.js
@@ -1,9 +1,16 @@
 import Login from "../pages/Login";
 import API from "./axios";
 
-export default function Login(WorkId,password){
-    return API.post()         /* // this is where we will send our credentials to check that we need to have 
-    // will return the body in JSON of data that we need.
-    need to write API call  to get user infomation..
-    */
-}
+import API from "./axios";
+
+export const loginUser = async (workId, password) => {
+    try {
+        const response = await API.post('/api/auth/login', {
+            WorkId: workId,
+            Password: password
+        });
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
