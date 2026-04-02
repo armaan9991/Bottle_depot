378ee778a32842c041f7a27f4e48e7fc4fcbfc01
Armaan Gill <armaan.gill1@ucalgary.ca>
2026-04-01 23:56:19 -0600

    implemented Schedule

--- BottleDepot/Models/Schedule.cs
+++ BottleDepot/Models/Schedule.cs
@@ -1,1 +1,13 @@
-// Testing
+namespace BottleDepot.Schdule
+{
+ public class  Schdule
+ {
+    public int SchduleID {get;set;}
+    public int WorkID {get;set;}
+    public DateTime ShiftDate {get; set;}
+    public decimal ShiftDuration {get; set;}
+    public bool IsBusy {get; set;}
+    public TimeSpan ShiftEnd {get; set;}
+    public TimeSpan ShiftStart {get; set;}
+ }
+}
