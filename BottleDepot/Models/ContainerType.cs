2bef6898dd022c936d5b4f24378f178b80e19e3f
Armaan Gill <armaan.gill1@ucalgary.ca>
2026-04-01 23:29:46 -0600

    added containerType model

--- BottleDepot/Models/ContainerType.cs
+++ BottleDepot/Models/ContainerType.cs
@@ -1,1 +1,11 @@
-// Testing
+namespace BottleDepot.Models
+{
+    public class ContainerType
+    {
+        public int ContainerTypeID { get ; set ; }
+        public decimal Refund {get ; set ;}
+        public string CountMethod { get ; set ; }
+        public decimal Size_of_Container { get ; set ; }
+        public string Material {get; set;}
+    }
+}
