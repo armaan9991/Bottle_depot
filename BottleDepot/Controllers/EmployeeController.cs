2de126866fa72d5ed8529b9e70ac50317ba8360c
Armaan Gill <armaan.gill1@ucalgary.ca>
2026-04-02 16:09:47 -0600

    added get employee by id

--- BottleDepot/Controllers/EmployeeController.cs
+++ BottleDepot/Controllers/EmployeeController.cs
@@ -38,6 +38,7 @@ namespace BottleDepot.Controllers
                 var Emp = new List<EmployeeDTO>();
 
                 var reader = await qry.ExecuteReaderAsync();
+                // reader gets rows
 
                 while ( await reader.ReadAsync())
                 {
@@ -69,5 +70,66 @@ namespace BottleDepot.Controllers
                 await _db.CloseAsync();
             }
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                await _db.OpenAsync();
+
+                var qry = new MySqlCommand(@"
+                SELECT
+                        e.WorkID,
+                        e.Name,
+                        e.Email,
+                        e.Phone,
+                        e.Role,
+                        e.WageRate,
+                        e.DateOfHire,
+                        e.SupervisorID,
+                        s.Name AS SupervisorName
+                    FROM EMPLOYEE e
+                    LEFT JOIN EMPLOYEE s
+                        ON e.SupervisorID = s.WorkID
+                    WHERE e.WorkID = @id", _db);
+                
+                qry.Parameters.AddWithValue("@id",id);
+
+                var reader = await qry.ExecuteReaderAsync();
+
+                if (!await reader.ReadAsync())
+                {
+                    return NotFound(new{message="Employee not found!"});
+                }
+
+                var emp = new EmployeeDTO
+                {
+                    WorkID         = reader.GetInt32("WorkID"),
+                    Name           = reader.GetString("Name"),
+                    Email          = reader.GetString("Email"),
+                    Phone          = reader.GetString("Phone"),
+                    Role           = reader.GetString("Role"),
+                    WageRate       = reader.GetDecimal("WageRate"),
+                    DateOfHire     = reader.GetDateTime("DateOfHire"),
+                    SupervisorID   = reader.IsDBNull(reader.GetOrdinal("SupervisorID"))
+                                         ? null
+                                         : reader.GetInt32("SupervisorID"),
+                    SupervisorName = reader.IsDBNull(reader.GetOrdinal("SupervisorName"))
+                                         ? null
+                                         : reader.GetString("SupervisorName")
+                };
+                return Ok(emp);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new {message = e.Message});
+            }
+            finally
+            {
+                await _db.CloseAsync();
+            }
+        }
+    
     }
 }
