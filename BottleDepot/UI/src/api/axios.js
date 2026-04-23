cb2a074b2670eac1c0178ab423cf2290f1dd9097
Armaan Gill <armaan.gill1@ucalgary.ca>
2026-04-22 21:58:38 -0600

    time

--- BottleDepot/UI/src/api/axios.js
+++ BottleDepot/UI/src/api/axios.js
@@ -22,16 +22,16 @@ API.interceptors.request.use(
     },
     (error) => Promise.reject(error)
 );
-// API.interceptors.response.use(
-//     (response) => response,
-//     (error) => {
-//         if (error.response?.status === 401) {
-//             localStorage.removeItem('jwt_token');
-//             localStorage.removeItem('user');
-//             window.location.href = '/';
-//         }
-//         return Promise.reject(error);
-//     }
-// );
+API.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem('jwt_token');
+            localStorage.removeItem('user');
+            window.location.href = '/';
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default API;
